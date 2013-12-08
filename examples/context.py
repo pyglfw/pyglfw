76f5335ca11239db69f44f3a668d18eb16524a5d
Roman Valov <reddot@nayadra.com>
2013-12-09 00:13:29 +0400

    ref: simplified context example.

--- examples/context.py
+++ examples/context.py
@@ -5,20 +5,22 @@ from pyglfw import *
 if __name__ == '__main__':
     init()
 
-    for cver in [ (3,3,True), (3,2,True), (3,1,False), (3,0,False) ]:
+    for vermaj, vermin, iscore in [ (3,3,True), (3,2,True), (3,1,False), (3,0,False) ]:
         try:
-            if cver[2]:
-                Window.hint(context_version=cver[:2],forward_compat=True,opengl_profile=Window.CORE_PROFILE)
-            else:
-                Window.hint(context_version=cver[:2])
+            Window.hint(context_version=(vermaj,vermin))
+            if iscore:
+                Window.hint(forward_compat=True)
+                Window.hint(opengl_profile=Window.CORE_PROFILE)
             w = Window(800, 600, "Тест: %s" % api_version_string())
             break
-        except (PlatformError, VersionUnavailableError) as e:
-            print("%s %s" % (cver, e))
-
-    w.make_current()
+        except (PlatformError, VersionUnavailableError, ValueError) as e:
+            print("%s.%s %s: %s" % (vermaj, vermin, 'CORE' if iscore else '', e))
+    else:
+        raise SystemExit("Proper OpenGL 3.x context not found")
 
     print(w.context_version)
+
+    w.make_current()
 
     k = w.keys
 
