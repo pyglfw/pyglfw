ab25d60dbe04a0895a18d3e2bbf05b2e0b904fb0
Roman Valov <reddot@nayadra.com>
2013-12-13 09:53:16 +0400

    new: more info for setup.py.

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from distutils.core import setup
 
 setup(name='pyglfw',
       version='3.0',
+      description='Python bindings for the GLFW library',
+      author='Roman Valov',
+      author_email='roman.valov@gmail.com',
+      license='zlib',
       py_modules=['libglfw', 'pyglfw'],
       )
 
