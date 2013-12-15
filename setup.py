97322293d514cb00465f8c79b50c164b9ce4257c
Roman Valov <reddot@nayadra.com>
2013-12-15 15:29:41 +0400

    new: add classifiers to setup.py.

--- setup.py
+++ setup.py
@@ -7,5 +7,20 @@ setup(name='pyglfw',
       author_email='roman.valov@gmail.com',
       license='zlib',
       packages=['pyglfw', 'pyglfw.libapi'],
+      classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: zlib/libpng License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Multimedia',
+        'Topic :: Games/Entertainment',
+        'Topic :: Software Development :: Libraries',
+        )
       )
 
