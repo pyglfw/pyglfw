569ec5c04d8c89864251c4e72e678c48c88eeb89
Roman Valov <reddot@nayadra.com>
2014-05-03 17:29:07 +0400

    new: update context.py with swap_current.

--- examples/context.py
+++ examples/context.py
@@ -24,26 +24,13 @@ if __name__ == '__main__':
 
     print(w.context_version)
 
-    def make_context_current(context):
-        try:
-            import types
-            f_type = types.FunctionType
-        except:
-            import new
-            f_type = new.function
-
-        make_current = Window.make_current
-        if not isinstance(make_current, f_type):
-            make_current = make_current.__func__
-        return make_current(context)
-
     assert Window.find_current() is None
 
-    make_context_current(w)
+    Window.swap_current(w)
 
     assert Window.find_current() is w
 
-    make_context_current(None)
+    Window.swap_current(None)
 
     assert Window.find_current() is None
 
